71aede0f2178408f4b23e382a9433465a0afc5dd
crocospare <student.corentin.turgis@gmail.com>
2021-12-06 17:54:55 +0100

    adding pick_word() function

---
+++ pick_word.py
@@ -0,0 +1,21 @@
+import random
+
+
+def count_line(fr_list):
+    line_count = 0
+    for line in fr_list:
+        if line != '\n':
+            line_count += 1
+    fr_list.seek(0, 0)
+    return line_count
+
+
+def pick_word():
+    fr_list = open("dictionary/fr_dict.txt", "r")
+    line_count = count_line(fr_list)
+    random_number = random.randint(1, line_count)
+    i = 0
+    while i < random_number:
+        random_line = fr_list.readline()
+        i += 1
+    return random_line.lower()
